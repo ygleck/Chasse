529a8e45b2332c9e1135a81072419e7c5e71913a
ygleck <ygleck@gmail.com>
2025-12-07 00:50:27 -0500

    Fix: ordre use client sur upload

--- app/(public)/upload/page.tsx
+++ app/(public)/upload/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
 'use client';
 
+import Image from 'next/image';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { useState } from 'react';
